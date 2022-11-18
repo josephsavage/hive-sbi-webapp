ca44b17f0d6d6bcf6389b18d7fef3461ca1ff27f
Andrés Quiroga <andres.quiroga6041@gmail.com>
2022-11-18 14:23:17 -0500

    Added expnadible modal votes information

--- app/hive_sbi_webapp/webapp/static/assets/js/delivered_votes.js
+++ app/hive_sbi_webapp/webapp/static/assets/js/delivered_votes.js
@@ -2,8 +2,19 @@ $(function () {
   'use strict';
   
   $(".delivered_votes_modal_link").on("click", function() {
-    console.log("HOLA")
     let post_title = $(this).closest("tr").data("title")
+
     $("#deliveredVotesModalLabel").text(post_title)
+    $("#idPostVotesTBody").html("")
+
+    let post_votes = ($(this).closest("tr").data("post-votes"))
+    let post_votes_tbody_html = ""
+
+    post_votes.forEach(function (vote, index) {
+      let vote_tr = "<tr><td>" + vote.voter + "</td><td>" + vote.hive_power_rewards + "</td><td>" + vote.hbd_rewards + "</td></tr>"
+      post_votes_tbody_html = post_votes_tbody_html + vote_tr
+
+      $("#idPostVotesTBody").html(post_votes_tbody_html)
+    });
   })
 })
