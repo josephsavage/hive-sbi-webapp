7c14421d63ea85dc9aad42a93a9c38d3b34e2cbb
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2025-09-17 15:53:08 -0600

    Create metabase.py

---
+++ app/utils/metabase.py
@@ -0,0 +1,13 @@
+import time, jwt, os
+
+METABASE_SITE_URL = os.environ.get("METABASE_SITE_URL")
+METABASE_SECRET_KEY = os.environ.get("METABASE_SECRET_KEY")
+
+def signed_dashboard_url(dashboard_id, params=None):
+    payload = {
+        "resource": {"dashboard": dashboard_id},
+        "params": params or {},
+        "exp": round(time.time()) + (60 * 10)  # 10 min expiry
+    }
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+    return f"{METABASE_SITE_URL}/embed/dashboard/{token}#bordered=true&titled=true"
