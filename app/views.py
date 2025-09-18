57d75fcdacaf5b86cbcd448c9bf122187d3063bf
Joseph Savage <34017974+josephsavage@users.noreply.github.com>
2025-09-18 08:22:46 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .utils.metabase import signed_dashboard_url
 
-def rich_list(request):
-    iframe_url = signed_dashboard_url(5)  # replace 5 with your dashboard ID
-    return render(request, "rich_list.html", {"iframe_url": iframe_url})
+def richlist_view(request):
+    iframe_url = "https://www.hivesbi.com/richlist/"
+    return render(request, "richlist.html", {"iframe_url": iframe_url})
